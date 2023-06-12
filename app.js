5ed1c38d25b36a355ca5bd4d5e64cf6ffd6cec94
aamathew <aamathew51293@gmail.com>
2023-06-12 16:35:09 -0500

    Project update

--- app.js
+++ app.js
@@ -3,33 +3,19 @@ $(document).ready(function() {
       e.preventDefault(); // Prevent form submission
   
       // Get form data
-      var name = $("#name").val();
-      var email = $("#email").val();
-      var message = $("#message").val();
-  
-      // Perform form validation (you can add more validation if needed)
-  
-      // Send form data using AJAX
-      $.ajax({
-        url: "https://formspree.io/f/xyylngw",
-        method: "POST",
-        data: {
-          name: name,
-          email: email,
-          message: message
-        },
-        dataType: "json",
-        success: function(response) {
-          // Handle success response
-          console.log("Form submitted successfully");
-          // You can display a success message or redirect to another page here
-        },
-        error: function(xhr, status, error) {
-          // Handle error response
-          console.log("Form submission failed");
-          // You can display an error message here
-        }
-      });
+      const personName = $("#name").val();
+
+      const thankYouMessageElements = $('#thank-you-message');
+      const firstThankYouMessageElement = thankYouMessageElements[0];
+      const doesThankYouMessageElementExist = firstThankYouMessageElement !== undefined;
+
+      if (doesThankYouMessageElementExist) {
+        $('#thank-you-message').remove()
+      }
+
+      $('.contact-form-container').append(`<p id="thank-you-message">Thank you ${personName}</p>`)
+      $('#thank-you-message').hide();
+      $('#thank-you-message').fadeIn();
     });
 
   });
